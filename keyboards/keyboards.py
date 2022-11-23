594ae17e0d8f673faea0cad93460813ec6af8f10
Joelwallker <cominghome120@gmail.com>
2022-11-23 10:03:08 +0300

    add requirements.txt

--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -1,7 +1,7 @@
 from aiogram.types import ReplyKeyboardMarkup,  \
     InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardRemove
 
-
+# buttons
 actbutton1 = InlineKeyboardButton(text='Найти дежурного', callback_data='find_button')
 actbutton2 = InlineKeyboardButton(text='Пнуть дежурного', callback_data='sand_button')
 
